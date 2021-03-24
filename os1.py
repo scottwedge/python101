e59e2f3db3b2d61d30951faaf5130e0c7e65be72
Scott Wedge <scott.wedge@gmail.com>
2021-03-23 21:45:09 -0400

    Check if directory exists; Create lots of files using 'os' module

--- os1.py
+++ os1.py
@@ -1,3 +1,11 @@
 import os
 
-os.mkdir("/tmp/tmp")
+
+max = 1000
+for j in range(max):
+    if not os.path.exists("/tmp/tmp"):
+        os.mkdir("/tmp/tmp", mode=755)
+
+    # create subdirectory
+    f = open("/tmp/tmp/tmp" + str(j), "w")
+    f.close()
