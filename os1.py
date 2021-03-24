912175aef5f19ba1b811a7fe5907d0c2265c8f25
Scott Wedge <scott.wedge@gmail.com>
2021-03-23 23:52:29 -0400

    Import random; Create random file name with random letter and number

--- os1.py
+++ os1.py
@@ -2,8 +2,9 @@
 
 import os
 import string
+import random
 
-max = 100
+max = 100000
 print("MAX = {}".format(max))
 
 alphabet = string.ascii_lowercase  # Create string of abc...xyz
@@ -12,7 +13,13 @@ print("Alphabet is: {}".format(alphabet))
 if not os.path.exists("/tmp/tmp"):
     os.mkdir("/tmp/tmp", mode=755)
 
-for j in alphabet:
-    for num in range(max):
-        f = open("/tmp/tmp/tmp" + str(j) + str(num), "w")
-        f.close()
+for j in range(max):
+    random.seed()
+    index = random.randint(0, len(alphabet) - 1)
+    letter = list(alphabet)[index]
+        
+    index = random.randint(0, max)
+    num = index
+    
+    f = open("/tmp/tmp/" + str(letter) + str(num), "w")   # create file
+    f.close()  # close file
