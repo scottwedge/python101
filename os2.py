0764c4d57b53fcdab1771d205c7c82324fedb951
Scott Wedge <scott.wedge@gmail.com>
2021-03-24 22:29:51 -0400

    Add blank lines to improve formatting of print and write output

--- os2.py
+++ os2.py
@@ -22,10 +22,12 @@ for j in list_of_functions:
     if "cwd" in j:
         list_of_cwd.append(j)
 
-print("functions with 'cwd' in them are: ", list_of_cwd)
+print() # blank line
+print("Functions with 'cwd' in them are: ", list_of_cwd)
 
 f = open(FILE, "w")
 f.writelines("List of functions in 'os' module:" + NEWLINE)
 f.writelines(str(list_of_functions) + NEWLINE)
-f.writelines("functions with 'cwd' in them are: " + NEWLINE)
+f.writelines(NEWLINE)
+f.writelines("Functions with 'cwd' in them are: " + NEWLINE)
 f.writelines(str(list_of_cwd) + NEWLINE)
