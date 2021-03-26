66e903853cb0d178f34d0ff8508998e1f87c3180
Scott Wedge <scott.wedge@gmail.com>
2021-03-25 23:57:48 -0400

    Create loop to create multiple directory names; Then create all directories at once with 'os.makedirs'

--- os-makedirs.py
+++ os-makedirs.py
@@ -14,7 +14,8 @@ import string
 # Constants
 BASE = "/tmp/tmp"  # base directory
 MODE = 0x755
-MAX = 9999
+MAX_NUM = 9999
+MAX_DIR_DEPTH = 100
 
 lower_alphabet = string.ascii_lowercase
 upper_alphabet = string.ascii_uppercase
@@ -22,15 +23,21 @@ upper_alphabet = string.ascii_uppercase
 full_alphabet = lower_alphabet + upper_alphabet
 old_name = BASE
 
-# Create directory name of random length of random alphabet characters
-random_length = random.randint(1,10)
-new_name = ""  # initialize name 
-for j in range(random_length):
-    index = random.randint(0, len(full_alphabet) - 1)
-    new_name = new_name + list(full_alphabet)[index]
+for k in range(MAX_DIR_DEPTH):
+    # Create directory name of random length of random alphabet characters
+    random_length = random.randint(1,10)
+    new_name = ""  # initialize name 
+    for j in range(random_length):
+        index = random.randint(0, len(full_alphabet) - 1)
+        new_name = new_name + list(full_alphabet)[index]
+    
+    # Then append random number to directory name
+    new_name = new_name + str(random.randint(0,MAX_NUM))
+    print("NEW DIR =", new_name)  # DEBUG
+    
+    full_path = os.path.join(old_name, new_name)
+#    os.makedirs(full_path, MODE)   # create new directory
+    old_name = full_path
 
-# Then append random number to directory name
-new_name = new_name + str(random.randint(0,MAX))
-
-full_path = os.path.join(old_name, new_name)
-os.makedirs(full_path, MODE)
+print("NEW FULL PATH=", full_path)
+os.makedirs(full_path, MODE)   # create new directory
