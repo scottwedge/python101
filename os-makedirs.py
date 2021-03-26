8f0be97ba7f369b47f8d16643744f74ef6c1ef74
Scott Wedge <scott.wedge@gmail.com>
2021-03-25 23:40:34 -0400

    Create directory name of random length with random letters and number; then create directory using 'os.makedirs'

--- os-makedirs.py
+++ os-makedirs.py
@@ -2,6 +2,7 @@
 
 # Fooling around with os.makedirs()
 # Create a long string of sub-directories
+# need to use mode of "0oxxx" instead of "xxx" according to help
 
 # Imports
 import os
@@ -10,10 +11,26 @@ import string
 
 # under "/tmp/tmp/" create long directories path
 
+# Constants
 BASE = "/tmp/tmp"  # base directory
+MODE = 0x755
+MAX = 9999
 
-lower_alphabet = string.ascii_lowercase()
-upper_alphabet = string.ascii_uppercase()
+lower_alphabet = string.ascii_lowercase
+upper_alphabet = string.ascii_uppercase
 
+full_alphabet = lower_alphabet + upper_alphabet
+old_name = BASE
 
+# Create directory name of random length of random alphabet characters
+random_length = random.randint(1,10)
+new_name = ""  # initialize name 
+for j in range(random_length):
+    index = random.randint(0, len(full_alphabet) - 1)
+    new_name = new_name + list(full_alphabet)[index]
 
+# Then append random number to directory name
+new_name = new_name + str(random.randint(0,MAX))
+
+full_path = os.path.join(old_name, new_name)
+os.makedirs(full_path, MODE)
