02d901b2195839f54f827c61107199591b6051e0
Scott Wedge <scott.wedge@gmail.com>
2021-03-25 22:57:39 -0400

    Initial commit; Comments with expected behaviour

---
+++ os-makedirs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Fooling around with os.makedirs()
+# Create a long string of sub-directories
+
+# Imports
+import os
+import random
+import string
+
+# under "/tmp/tmp/" create long directories path
+
+BASE = "/tmp/tmp"  # base directory
+
+lower_alphabet = string.ascii_lowercase()
+upper_alphabet = string.ascii_uppercase()
+
+
+
