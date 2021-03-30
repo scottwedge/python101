95c49451d365c608c13f20b97b0c6a17627b0549
Scott Wedge <scott.wedge@gmail.com>
2021-03-29 23:57:30 -0400

    Still endless loop

--- os-delete-most.py
+++ os-delete-most.py
@@ -24,34 +24,36 @@ else:
 
 all_dir = []   # DEBUG
 
-base_list_of_file_or_dir = os.listdir()   # initialize empty lists
+base_list_of_file_or_dir = os.listdir()   # initialize initial list of directory contents
 
 for j in list(base_list_of_file_or_dir):
 
     still_deleting = True
     
     while still_deleting:
-        print("Currently in directory: {}".format(os.getcwd()))   # DEBUG
-        list_of_file_or_dir = os.listdir()
-        all_dir.append(list_of_file_or_dir)   # DEBUG
-        print("Contains: {}".format(list_of_file_or_dir))   # DEBUG
-
         if os.path.isfile(j): # if is file then delete it
             os.remove(j)
             print("Just removed:", j)
+            still_deleting = False  # exit while loop
         elif os.path.isdir(j): # if directory then go to it
             os.chdir(j)
             print("Just changed to directory", j)
         else:   # other than file or directory
-            pass
+            pass   # what case could this be??
 
-        if list_of_file_or_dir == []:  # At lowest subdirectory, is empty
-            if os.getcwd() == BASE:
-                still_deleting = False
-            else:
-                os.chdir("..")   # move up one directory
-                contents = os.listdir()
-                os.rmdir(contents)
+        # Now in new subdirectory
+        print("Currently in directory: {}".format(os.getcwd()))   # DEBUG
+        list_of_file_or_dir = os.listdir()    # DEBUG
+        all_dir.append(list_of_file_or_dir)   # DEBUG
+        print("Contains: {}".format(list_of_file_or_dir))   # DEBUG
+
+#        if list_of_file_or_dir == []:  # At lowest subdirectory, is empty
+#            if os.getcwd() == BASE:
+#                still_deleting = False
+#            else:
+#                os.chdir("..")   # move up one directory
+#                contents = os.listdir()
+#                os.rmdir(contents)
 
 print("All dir=", all_dir)
 print("DONE!")
